ef406cde3d6358fb9018f99ab816bf0ca6c784e0
Parsa_Shabanpour <parsash1380@gmail.com>
2024-07-31 10:36:40 +0330

    implement the customer entity class

---
+++ src/main/java/org/example/Customer.java
@@ -0,0 +1,39 @@
+package org.example;
+
+// marks this class as an entity and maps it to the "customer" table in db.
+@Entity(tableName = "customer")
+public class Customer {
+    @Id
+    @Column(name = "customerId")
+    private int id;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
